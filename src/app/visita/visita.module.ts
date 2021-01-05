6fca78f85eeb03745f19d80033ee566762acd476
Sebastian Garcia <sebastian.garcia.m01@mail.pucv.cl>
2021-01-05 00:32:53 -0300

    todo lo restante agregado

--- src/app/visita/visita.module.ts
+++ src/app/visita/visita.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VisitaRoutingModule } from './visita-routing.module';
+import { VisitaComponent } from './visita.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [VisitaComponent],
   imports: [
     CommonModule,
     VisitaRoutingModule
