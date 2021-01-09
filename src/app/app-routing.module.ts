40f19d0ee7ca202a0df6881e4440e1c8124e00cc
Sebastian Garcia <sebastian.garcia.m01@mail.pucv.cl>
2021-01-08 22:44:39 -0300

    creado vista login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./visita/visita.module').then(module => module.VisitaModule) 
+  },
+  {
+    path:'matchmaking',
+    loadChildren: () => import('./usuario/usuario.module').then(module => module.UsuarioModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/visita/visita.module.ts
+++ src/app/visita/visita.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { VisitaRoutingModule } from './visita-routing.module';
 import { VisitaComponent } from './visita.component';
+import { RegistroComponent } from './views/registro/registro.component';
+import { LoginComponent } from './views/login/login.component';
 
 
 @NgModule({
-  declarations: [VisitaComponent],
+  declarations: [VisitaComponent, RegistroComponent, LoginComponent],
   imports: [
     CommonModule,
     VisitaRoutingModule
