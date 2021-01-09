bf2e93a703dba9eae52ee380dc9fa6d5b2d60e96
Sebastian Garcia <sebastian.garcia.m01@mail.pucv.cl>
2021-01-08 23:05:55 -0300

    modulos listos

--- src/app/visita/visita-routing.module.ts
+++ src/app/visita/visita-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './views/registro/registro.component';
+import { VisitaComponent } from './visita.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component: VisitaComponent,
+  children:[
+    {
+      path:'registro',
+      component:RegistroComponent
+    }
+]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
