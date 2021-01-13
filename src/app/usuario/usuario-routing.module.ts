3183743aa9e3a75b38627833e721130a270d70a6
Sebastian Garcia <sebastian.garcia.m01@mail.pucv.cl>
2021-01-13 00:09:56 -0300

    agregado uso de rutas

--- src/app/usuario/usuario-routing.module.ts
+++ src/app/usuario/usuario-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsuarioComponent } from './usuario.component';
 import { HomeComponent } from './views/home/home.component';
+import { MatchComponent } from './views/match/match.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path:'home',
         component:HomeComponent
+      },
+      {
+        path:'match',
+        component:MatchComponent
       }
     ]
   }
