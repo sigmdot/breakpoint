5e8511e8488024f2b613c416105fa2f3f946771b
Sebastian Garcia <sebastian.garcia.m01@mail.pucv.cl>
2021-01-12 21:16:26 -0300

    creado modulo core

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+
+import {FirebaseModule} from './modules/firebase/firebase.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FirebaseModule
+  ]
+})
+export class CoreModule { }
