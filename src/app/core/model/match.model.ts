e65aa9a917d0df8ebee1590f550c76e4d8f0789d
Sebastian Garcia <sebastian.garcia.m01@mail.pucv.cl>
2021-01-17 02:14:55 -0300

    creado model matchs y el providers configurado para recibir datos

---
+++ src/app/core/model/match.model.ts
@@ -0,0 +1,8 @@
+export interface Match{
+    id?:string;
+    nombreMatch: string;
+    usuarioPublica: string;
+    items: boolean;
+    time: number;
+    estado: string;
+}
--- src/app/core/service-providers/matchs/matchs.service.ts
+++ src/app/core/service-providers/matchs/matchs.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Match } from '../../model/match.model';
+import { FirestoreService } from '../../services/firestore/firestore.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchsService {
 
-  constructor() { }
+  constructor(private firestoreSvc: FirestoreService) { }
+
+  public getAllPublicMatch(){
+    return this.firestoreSvc.snapshotCollection<any>('matchs')
+  }
+
+  public getMatchsByStatus(estado: string){
+    // tslint:disable-next-line: whitespace
+    return this.firestoreSvc.snapshotCollection<any>('matchs', ref => ref.where('estado', '==', estado));
+  }
+
+  public createMatch(match: Match){
+    match.id = this.firestoreSvc.creatId();
+    return this.firestoreSvc.addDocument('matchs', match.id, match);
+  }
 }
